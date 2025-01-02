4aba696f3b4f2bfbc6c2e5f7c5f52ecae363d26c
Cadu Souza <cadusouza14@gmail.com>
2025-01-01 22:57:05 -0300

    Added a very crude and inefficient way of zooming in and out

--- graph.py
+++ graph.py
@@ -1,31 +1,41 @@
+from typing import Any, Callable, Generator
+
 import pygame
 
 
 class Function:
     def __init__(
-        self, name: str, color: pygame.Color, points: list[tuple[float, float]]
+        self,
+        name: str,
+        f: Callable[[float], float],
+        color: pygame.Color,
+        points: list[tuple[float, float]],
     ) -> None:
         self.name = name
         self.color = color
         self.points = points
+        self.f = f
 
 
 class Graph:
     def __init__(self):
         self.functions: list[Function] = []
+        self.scale: float = 1
 
     def add_function(
         self,
         name: str,
-        f,
-        frange: tuple[int, int],
+        f: Callable[[float], float],
+        xrange_start: int,
+        xrange_end: int,
         color: pygame.Color = pygame.Color(255, 0, 0),
     ):
         self.functions.append(
             Function(
                 name,
+                f,
                 color,
-                [(x, f(x)) for x in range(frange[0], frange[1])],
+                [(x, f(x)) for x in range(xrange_start, xrange_end)],
             )
         )
 
@@ -44,9 +54,29 @@ class Graph:
         return list(
             map(
                 lambda point: (
-                    surface.get_width() / 2 + point[0],
-                    surface.get_height() / 2 - point[1],
+                    surface.get_width() / 2 + point[0] * self.scale,
+                    surface.get_height() / 2 - point[1] * self.scale,
                 ),
                 points,
             )
         )
+
+    def _generate_points(
+        self, start: float, end: float, step: float
+    ) -> Generator[float, Any, None]:
+        while start <= end:
+            yield start
+            start += step
+
+    def update_function_points(self):
+        for i, function in enumerate(self.functions):
+            self.functions[i].points = list(
+                map(
+                    lambda x: (x, function.f(x)),
+                    self._generate_points(
+                        function.points[0][0],
+                        function.points[-1][0],
+                        1 / self.scale,
+                    ),
+                )
+            )
--- main.py
+++ main.py
@@ -15,16 +15,27 @@ function_graph = graph.Graph()
 function_graph.add_function(
     "x^2",
     lambda x: x**2,
-    (-graph_surface.get_width(), graph_surface.get_width()),
+    -graph_surface.get_width(),
+    graph_surface.get_width(),
     pygame.Color(255, 255, 0),
 )
 
-function_graph.add_function(
-    "x",
-    lambda x: x,
-    (-graph_surface.get_width(), graph_surface.get_width()),
-    pygame.Color(0, 255, 0),
-)
+# function_graph.add_function(
+#     "x",
+#     lambda x: x,
+#     -graph_surface.get_width(),
+#     graph_surface.get_width(),
+#     pygame.Color(0, 255, 0),
+# )
+
+
+# function_graph.add_function(
+#     "-x",
+#     lambda x: -x,
+#     -graph_surface.get_width(),
+#     graph_surface.get_width(),
+#     pygame.Color(0, 0, 255),
+# )
 
 arrow_sidelength = 10
 arrow_height = arrow_sidelength * (3**0.5) / 2
@@ -92,21 +103,32 @@ y_axis_coords = (
 clock = pygame.time.Clock()
 running = True
 
+scale_factor = 1.1
+
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_z:
+                function_graph.scale *= scale_factor
+                function_graph.update_function_points()
+            elif event.key == pygame.K_x:
+                function_graph.scale /= scale_factor
+                function_graph.update_function_points()
 
+            print(len(function_graph.functions[0].points))
+
+    graph_surface.fill((0, 0, 0))
     screen.fill((0, 0, 0))
 
     pygame.draw.lines(graph_surface, axis_color, True, x_axis_coords)
     pygame.draw.lines(graph_surface, axis_color, True, y_axis_coords)
-    function_graph.blit_functions(graph_surface)
-    # pygame.draw.lines(graph_surface, points_color, True, translated_points)
     pygame.draw.polygon(graph_surface, axis_color, x_axis_up_arrow)
     pygame.draw.polygon(graph_surface, axis_color, x_axis_down_arrow)
     pygame.draw.polygon(graph_surface, axis_color, y_axis_left_arrow)
     pygame.draw.polygon(graph_surface, axis_color, y_axis_right_arrow)
+    function_graph.blit_functions(graph_surface)
 
     screen.blit(graph_surface, (graph_x_offset / 2, graph_y_offset / 2))
 
